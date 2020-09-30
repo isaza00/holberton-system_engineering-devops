f58d4e6de7ea8678b87d8405d22fd4d5782fc89d
Norman <isaza00@hotmail.com>
2020-09-30 16:34:42 -0500

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,4 @@
-include stdlib
-
-file_line {'phpp':
-  ensure => present,
-  path   => '/var/www/html/wp-settings.php',
-  line   => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match  => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );"
+exec { 'update':
+  path    => '/bin',
+  command => "sed -i -e 's/phpp/php/g' /var/www/html/wp-settings.php",
 }

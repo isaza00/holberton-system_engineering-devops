22f55486d76e05c31abf6e72d713e258739fcba7
Norman <isaza00@hotmail.com>
2020-10-08 15:21:35 -0500

    1B

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Mandatory exercise
+
+exec { 'change ulimit':
+    path    => '/bin',
+    command => "sed -i 's/15/2000/g' /etc/default/nginx"
+}
+
+exec { 'nging restart':
+    path    => '/etc/init.d',
+    command => 'nginx restart'
+}
